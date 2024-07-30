57ac3fa8b17584c2710b03330e19ddf0a89b823d
Aavash Shrestha <aavashstha1212@gmail.com>
2024-07-30 16:47:23 +0545

    Date : 2020-10-06 17:00 PM Author : Aavash Shrestha Title : Added Profile Picture and Styling Description : Added Profile Picture and Styling to the Header Componen. Also added the Style.css and Style.scss file for the styling of the components. With new Profile Picture, the Header component looks more attractive and appealing.

--- LearningReact/src/Header.jsx
+++ LearningReact/src/Header.jsx
@@ -2,16 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Style.scss";
 import logo from "./assets/Logo.svg";
+import Profile from "./assets/Profile.png";
+
+const Login = false;
+function LoginStatus() {
+  if (Login == true) {
+    function LogedIn() {
+      return (
+        <div className="Header__ProfileMenu">
+          <Link to="/profile" className="Header__ProfileMenu-Profile">
+            <img src={Profile} alt="Profile" />
+          </Link>
+        </div>
+      );
+    }
+  } else if (Login == false) {
+    function LoginStatus() {
+      return (
+        <div className="Header__ProfileMenu">
+          <Link to="/login" className="Header__ProfileMenu-Login">
+            Login
+          </Link>
+        </div>
+      );
+    }
+  }
+}
 
 function Header() {
   return (
-    <div className="Header no-hover">
+    <div className="Header">
       <Link to="/index">
         <div className="Header__Logo">
           <img src={logo} alt="Logo" />
-          <label className="Name">
-            <h4>Hotel Shrestha</h4>
-          </label>
+          <h4>Hotel Shrestha</h4>
         </div>
       </Link>
       <div className="Header__Menu">
@@ -21,13 +45,7 @@ function Header() {
         <Link to="/location">Location</Link>
       </div>
       <div className="Header__ProfileMenu">
-        {/* Profile and login links */}
-        <Link to="/login" className="Header__Login">
-          Login
-        </Link>
-        <Link to="/profile" className="Header__Profile">
-          Profile
-        </Link>
+        <LoginStatus />
       </div>
     </div>
   );
