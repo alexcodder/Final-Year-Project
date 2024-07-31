cf2935b8a82610c77097339e2f234135f7e5a4c2
Aavash Shrestha <aavashstha1212@gmail.com>
2024-07-31 15:15:13 +0545

    Date: Wednesday, 2024-07-31 3:15 PM Author: Aavash Shrestha Title: Day 2 Added Banner image Description: created a new banner image for the home page and added it to the home page.

--- LearningReact/src/pages/Index.jsx
+++ LearningReact/src/pages/Index.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import '../stylesheet/Style.scss';
+import React from "react";
+import "../stylesheet/Style.scss";
+import "../stylesheet/Home.scss";
 
 function Home() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
+    <div className="Homepage">
+      <div className="Homepage__banner1">
+        <div className="Homepage__banner1-text">
+          <h1>Hotel Shrestha</h1>
+          <p>Best place to stay in the city</p>
+        </div>
+      </div>
     </div>
   );
 }
 
-Home()
+Home();
 
 export default Home;
