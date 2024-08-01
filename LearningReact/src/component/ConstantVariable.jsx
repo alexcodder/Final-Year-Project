b9a6b65839dab33efd85761b8f8a94565c441dee
Aavash Shrestha <aavashstha1212@gmail.com>
2024-08-01 12:46:17 +0545

    Date: 2024-08-01 Aurthor: Aavash Shrestha Commit: Version-1.2 Day 3 Description: Updated Filter component and Header component

--- LearningReact/src/component/ConstantVariable.jsx
+++ LearningReact/src/component/ConstantVariable.jsx
@@ -1,13 +1,2 @@
-let user = 0; // Declare and initialize the user variable
-let LoginStatus; // Declare the LoginStatus variable
-
-export default function LoginCheck() {
-    if (user === 1) {
-        LoginStatus = true;
-    } else {
-        LoginStatus = false;
-    }
-    return LoginStatus;
-}
-
-export { LoginStatus };
+export let user = 0; // Declare and initialize the user variable
+export let LoginStatus = false; // Declare the LoginStatus variable
